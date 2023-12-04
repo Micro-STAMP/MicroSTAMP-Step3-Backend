963514d508916b9c4b42c06d0d5e6061660ebd76
Gabriel Piva <89921667+gabriel-piva@users.noreply.github.com>
2023-12-04 15:58:19 -0300

    dto de criacao de uca

---
+++ step3-backend/src/main/java/step3/dto/unsafe_control_action/UnsafeControlActionCreateDto.java
@@ -0,0 +1,13 @@
+package step3.dto.unsafe_control_action;
+
+import step3.entity.UCAType;
+
+public record UnsafeControlActionCreateDto(
+        String name,
+        Long control_action_id,
+        Long contex_id,
+        Long constraint_id,
+        Long hazard_id,
+        UCAType type
+) {
+}
