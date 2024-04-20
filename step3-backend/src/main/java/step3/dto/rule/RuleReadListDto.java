c07e29951943ec0b9d1fdb761c730e062c429618
Gabriel Francelino <gabriel.francelino@sou.unifal-mg.edu.br>
2024-04-20 18:47:09 -0300

    feat: tags para identificar rules

--- step3-backend/src/main/java/step3/dto/rule/RuleReadListDto.java
+++ step3-backend/src/main/java/step3/dto/rule/RuleReadListDto.java
@@ -5,16 +5,18 @@ import step3.entity.Rule;
 public record RuleReadListDto(
         Long id,
         String name,
-        String control_action_name
+        String control_action_name,
+        String tag
 ) {
 
     // Constructors -----------------------------------
 
     public RuleReadListDto(Rule rule) {
         this(
-            rule.getId(),
-            rule.getName(),
-            rule.getControlAction().getName()
+                rule.getId(),
+                rule.getName(),
+                rule.getControlAction().getName(),
+                rule.getTagName()
         );
     }
 
