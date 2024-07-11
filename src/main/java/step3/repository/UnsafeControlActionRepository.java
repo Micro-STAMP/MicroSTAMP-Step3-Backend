95c63f60a7bb703a74f53f97dd55d4b4ecedebce
Gabriel Francelino <gabriel.francelino@sou.unifal-mg.edu.br>
2024-07-11 12:57:59 -0300

    refact: troca a uca manual pela criada pela rule

--- src/main/java/step3/repository/UnsafeControlActionRepository.java
+++ src/main/java/step3/repository/UnsafeControlActionRepository.java
@@ -4,9 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import step3.entity.UnsafeControlAction;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UnsafeControlActionRepository extends JpaRepository<UnsafeControlAction, Long> {
     List<UnsafeControlAction> findByControlActionId(Long id);
     List<UnsafeControlAction> findByRuleTag(String tag);
     List<UnsafeControlAction> findByControlActionControllerId(Long id);
+    Optional<UnsafeControlAction> findFirstByName(String name);
 }
